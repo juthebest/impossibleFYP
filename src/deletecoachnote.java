afe3b8489dd0285a6a2fcf62f77c3a6f4291de7d
juthebest <judetyr@gmail.com>
2016-03-08 17:18:10 +0800

    hi

--- src/deletecoachnote.java
+++ src/deletecoachnote.java
@@ -32,6 +32,7 @@ public class deletecoachnote extends HttpServlet {
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
+	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		
@@ -100,6 +101,7 @@ public class deletecoachnote extends HttpServlet {
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
+	@Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
