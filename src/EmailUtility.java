c4f0f71443bc5ad895d6d48bc955261738dcb600
juthebest <judetyr@gmail.com>
2016-03-15 11:01:39 +0800

    hi

---
+++ src/EmailUtility.java
@@ -0,0 +1,47 @@
+import java.net.Authenticator;
+import java.net.PasswordAuthentication;
+import java.util.Properties;
+
+import org.apache.catalina.Session;
+
+import com.sun.xml.internal.messaging.saaj.packaging.mime.MessagingException;
+
+public class EmailUtility {
+	public static void sendEmail(String host, String port,
+            final String userName, final char[] password, String toAddress,
+            String subject, String message) throws AddressException,
+            MessagingException {
+ 
+        // sets SMTP server properties
+        Properties properties = new Properties();
+        properties.put("mail.smtp.host", host);
+        properties.put("mail.smtp.port", port);
+        properties.put("mail.smtp.auth", "true");
+        properties.put("mail.smtp.starttls.enable", "true");
+ 
+        // creates a new session with an authenticator
+        Authenticator auth = new Authenticator() {
+            public PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(userName, password);
+            }
+        };
+ 
+        Session session = Session.getInstance(properties, auth);
+ 
+        // creates a new e-mail message
+        Message msg = new MimeMessage(session);
+ 
+        msg.setFrom(new InternetAddress(userName));
+        InternetAddress[] toAddresses = { new InternetAddress(toAddress) };
+        msg.setRecipients(Message.RecipientType.TO, toAddresses);
+        msg.setSubject(subject);
+        msg.setSentDate(new Date());
+        msg.setText(message);
+ 
+        // sends the e-mail
+        Transport.send(msg);
+ 
+    }
+}
+
+}
